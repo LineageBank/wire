3c76ad9ff8c9f7bf6217d3504bd5ff6c75bc2177
Martin Zarate <martin.zarate@provisionsgroup.com>
2022-09-14 14:59:56 -0600

    feat: clean files and modules

--- cmd/achcli/main.go
+++ cmd/achcli/main.go
@@ -9,39 +9,25 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
-	"github.com/moov-io/ach"
+	"github.com/moov-io/wire"
 )
 
 var (
-	flagVerbose = flag.Bool("v", false, "Print verbose details about each ACH file")
-	flagVersion = flag.Bool("version", false, "Print moov-io/ach cli version")
-
-	flagDiff     = flag.Bool("diff", false, "Compare two files against each other")
-	flagFlatten  = flag.Bool("flatten", false, "Flatten batches in each file")
-	flagMerge    = flag.Bool("merge", false, "Merge files before describing")
-	flagReformat = flag.String("reformat", "", "Reformat an incoming ACH file to another format")
-
-	flagMask = flag.Bool("mask", false, "Mask/hide full account numbers")
-
-	programName = filepath.Base(os.Args[0])
+	flagReformat = flag.String("reformat", "", "Reformat an incoming Wire file to json format")
+	programName  = filepath.Base(os.Args[0])
 )
 
 func init() {
 	flag.Usage = func() {
-		fmt.Printf("Usage of ach (%s):\n", ach.Version)
+		fmt.Printf("Usage (%s):\n", wire.Version)
 		fmt.Printf("   usage: %s [<flags>] <files>", programName)
 		fmt.Println("")
+		fmt.Println("")
 		fmt.Println("Commands: ")
-		fmt.Printf("  %s -diff first.ach second.ach", programName)
-		fmt.Println("    Show the difference between two ACH files")
-		fmt.Printf("  %s -mask file.ach", programName)
-		fmt.Println("                Print file details with personally identifiable information partially removed")
 		fmt.Printf("  %s -reformat=json first.ach", programName)
-		fmt.Println("      Convert an incoming ACH file into another format (options: ach, json)")
-		fmt.Printf("  %s 20060102.ach", programName)
-		fmt.Println("                  Summarize an ACH file for human readability")
+		fmt.Println("  [ Convert an incoming Wire file into json format ]")
+
 		fmt.Println("")
 		fmt.Println("Flags: ")
 		flag.PrintDefaults()
@@ -51,35 +37,10 @@ func init() {
 func main() {
 	flag.Parse()
 
-	switch {
-	case *flagVersion:
-		fmt.Printf("moov-io/ach:%s cli tool\n", ach.Version)
-		return
-	case *flagVerbose:
-		fmt.Printf("moov-io/ach:%s cli tool\n", ach.Version)
-	}
-
 	args := flag.Args()
-
-	// error conditions, verify we're okay for whatever the task at hand is
-	switch {
-	case *flagDiff && len(args) != 2:
-		fmt.Printf("with -diff exactly two files are expected, found %d files\n", len(args))
-		os.Exit(1)
-	}
-
-	// minor debugging
-	if *flagVerbose {
-		fmt.Printf("found %d ACH files to describe: %s\n", len(args), strings.Join(args, ", "))
-	}
 
 	// pick our command to do
 	switch {
-	case *flagDiff:
-		if err := diffFiles(args); err != nil {
-			fmt.Printf("ERROR: %v\n", err)
-			os.Exit(1)
-		}
 
 	case *flagReformat != "" && len(args) == 1:
 		if err := reformat(*flagReformat, args[0]); err != nil {
--- cmd/achcli/reformat.go
+++ cmd/achcli/reformat.go
@@ -10,7 +10,7 @@ import (
 	"io"
 	"os"
 
-	"github.com/moov-io/ach"
+	"github.com/moov-io/wire"
 )
 
 func reformat(as string, filepath string) error {
@@ -24,13 +24,14 @@ func reformat(as string, filepath string) error {
 	}
 
 	switch as {
-	case "ach":
-		w := ach.NewWriter(os.Stdout)
-		if err := w.Write(file); err != nil {
-			return err
-		}
+	// case "json":
+	// 	w := wire.NewWriter(os.Stdout)
+	// 	if err := w.Write(file); err != nil {
+	// 		return err
+	// 	}
 
 	case "json":
+		// if err := json.RawMessage(os.Stdout).Encode(file); err != nil {
 		if err := json.NewEncoder(os.Stdout).Encode(file); err != nil {
 			return err
 		}
@@ -41,29 +42,35 @@ func reformat(as string, filepath string) error {
 	return nil
 }
 
-func readIncomingFile(path string) (*ach.File, error) {
+func readIncomingFile(path string) (*wire.File, error) {
 	file, err := readJsonFile(path)
 	if file != nil && err == nil {
 		return file, nil
 	}
-	file, err = readACHFile(path)
-	if file != nil && err == nil {
-		return file, nil
-	}
+
 	return nil, fmt.Errorf("unable to read %s:\n %v", path, err)
 }
 
-func readJsonFile(path string) (*ach.File, error) {
+func readJsonFile(path string) (*wire.File, error) {
+
 	fd, err := os.Open(path)
 	if err != nil {
 		return nil, fmt.Errorf("problem opening %s: %v", path, err)
 	}
 	defer fd.Close()
 
+	// fd, err := os.ReadFile(path)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("problem reading %s: %v", path, err)
+	// }
+	// str := string(fd)
+	// str2 := json.Encoder(str)
+	// println(str2)
+
 	bs, err := io.ReadAll(fd)
 	if err != nil {
 		return nil, fmt.Errorf("problem reading %s: %v", path, err)
 	}
 
-	return ach.FileFromJSON(bs)
+	return wire.FileFromJSON(bs)
 }
